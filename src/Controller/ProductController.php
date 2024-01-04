bd4f981d431a2f6bd2c407c229447646f628c41a
JosephDeFacto <josip.mareljic@gmail.com>
2024-01-04 21:27:40 +0100

    Create Product entity.

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,14 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/', name: 'app_product')]
-    public function index()
+
+    public ProductRepository $productRepository;
+
+    public function __construct(ProductRepository $productRepository)
     {
-        return $this->render('products/index.html.twig', []);
+        $this->productRepository = $productRepository;
+    }
+    #[Route('/', name: 'app_product')]
+    public function index(): Response
+    {
+        $products = $this->productRepository->findAll();
+
+        if (!$products) {
+            throw $this->createNotFoundException('No products');
+        }
+
+        return $this->render('products/index.html.twig', ['products' => $products]);
+    }
+
+    #[Route('product/{id}', name: 'app_product_id')]
+    public function show(Product $product): Response
+    {
+        return $this->render('products/show.html.twig' , ['product' => $product]);
     }
 }
