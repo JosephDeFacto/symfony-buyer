af3f9c886249ad273546a2913a4c6146da996bdf
JosephDeFacto <josip.mareljic@gmail.com>
2024-01-07 21:21:24 +0100

    Make setter for id.

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -45,6 +45,13 @@ class Product
         return $this->id;
     }
 
+    public function setId($id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
     public function getName(): ?string
     {
         return $this->name;
