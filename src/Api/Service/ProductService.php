dc26e40ea156d450dd5b213962431549572634d7
JosephDeFacto <josip.mareljic@gmail.com>
2024-01-09 14:27:11 +0100

    Associate Products with Categories.

--- src/Api/Service/ProductService.php
+++ src/Api/Service/ProductService.php
@@ -4,6 +4,7 @@ namespace App\Api\Service;
 
 use App\Entity\Product;
 use App\Api\ApiProductHelper;
+use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
@@ -14,11 +15,14 @@ class ProductService
 
     public EntityManagerInterface $entityManager;
 
-    public function __construct(HttpClientInterface $dummyProductJsonApiClient, ApiProductHelper $apiProductHelper, EntityManagerInterface $entityManager)
+    public CategoryRepository $categoryRepository;
+
+    public function __construct(HttpClientInterface $dummyProductJsonApiClient, ApiProductHelper $apiProductHelper, EntityManagerInterface $entityManager, CategoryRepository $categoryRepository)
     {
         $this->client = $dummyProductJsonApiClient;
         $this->apiProductHelper = $apiProductHelper;
         $this->entityManager = $entityManager;
+        $this->categoryRepository = $categoryRepository;
     }
 
     public function save(): void
@@ -27,12 +31,15 @@ class ProductService
 
         foreach ($products['products'] as $p) {
 
+            $category = $this->categoryRepository->findOneBy(['name' => $p['category']]);
+
             $product = new Product();
             $product->setId((int)$p['id']);
             $product->setName((string)$p['title']);
             $product->setDescription($p['description']);
             $product->setPrice($p['price']);
             $product->setStockQuantity($p['stock']);
+            $product->setCategory($category);
 
             $this->entityManager->persist($product);
 
