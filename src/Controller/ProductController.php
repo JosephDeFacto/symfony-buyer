e8e2c938cb2b20aae00fc7925bcd87d45dc6da59
JosephDeFacto <josip.mareljic@gmail.com>
2024-01-16 20:46:55 +0100

    Add product category page.

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,8 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
-use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\NonUniqueResultException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,10 +14,14 @@ class ProductController extends AbstractController
 {
 
     public ProductRepository $productRepository;
+    public CategoryRepository $categoryRepository;
 
-    public function __construct(ProductRepository $productRepository)
+
+    public function __construct(ProductRepository $productRepository, CategoryRepository $categoryRepository)
     {
         $this->productRepository = $productRepository;
+        $this->categoryRepository = $categoryRepository;
+
     }
     #[Route('/', name: 'app_product')]
     public function index(): Response
@@ -27,7 +32,23 @@ class ProductController extends AbstractController
             throw $this->createNotFoundException('No products');
         }
 
-        return $this->render('products/index.html.twig', ['products' => $products]);
+        $categories = $this->categoryRepository->findAll();
+
+        return $this->render('products/index.html.twig', ['products' => $products, 'categories' => $categories]);
+    }
+
+    /**
+     * @throws NonUniqueResultException
+     */
+    #[Route('/category/{name}', name: 'app_product_category')]
+    public function showByCategory(string $name): Response
+    {
+        $categories = $this->categoryRepository->findAll();
+        $category = $this->categoryRepository->findByName(['name' => $name]);
+
+        $categoryProducts = $category->getProducts();
+
+        return $this->render('products/product_category.html.twig', ['categoryProducts' => $categoryProducts, 'categories' => $categories]);
     }
 
     #[Route('product/{id}', name: 'app_product_id')]
