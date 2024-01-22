eaa6f5c155137bee856e248a35d2bab58bb42f55
JosephDeFacto <josip.mareljic@gmail.com>
2024-01-22 23:29:56 +0100

    Add addition 'User' fields.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -43,6 +43,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\OneToMany(mappedBy: 'user', targetEntity: Wishlist::class)]
     private Collection $wishlists;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastname = null;
+
     public function __construct()
     {
         $this->carts = new ArrayCollection();
@@ -218,6 +224,30 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
                 $wishlist->setUser(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(?string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): static
+    {
+        $this->lastname = $lastname;
 
         return $this;
     }
