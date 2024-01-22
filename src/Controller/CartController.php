27b42ec975ad856cb40fa1e45ba66778a980470a
JosephDeFacto <josip.mareljic@gmail.com>
2024-01-22 23:06:23 +0100

    Dynamic quantity.

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -7,13 +7,15 @@ use App\Entity\CartItem;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
     #[Route('/{id}/addToCart', name: 'app_add_to_cart')]
-    public function index(EntityManagerInterface $entityManager, Product $product): Response
+    public function index(EntityManagerInterface $entityManager, Product $product, Request $request): Response
     {
         $user = $this->getUser();
         $cart = new Cart();
@@ -23,14 +25,20 @@ class CartController extends AbstractController
         $entityManager->persist($cart);
 
         $cartItems = new CartItem();
+        $quantity = (int) $request->request->get('quantity');
+
+        if ($quantity < 1) {
+            $quantity = 1;
+        }
+
         $cartItems->setCart($cart);
         $cartItems->setProduct($product);
+        $cartItems->setQuantity($quantity);
 
         $entityManager->persist($cartItems);
-
         $entityManager->flush();
 
-        return new Response('New cart');
+        return $this->json(['quantity' => $cartItems->getQuantity()]);
 
 
     }
