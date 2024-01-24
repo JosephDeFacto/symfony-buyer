5ee171640c83bd6c97773e113dc42fd75b6e9093
JosephDeFacto <josip.mareljic@gmail.com>
2024-01-24 23:32:28 +0100

    Store quantity in session and display number of items in cart icon.

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -7,17 +7,20 @@ use App\Entity\CartItem;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
+
     #[Route('/{id}/addToCart', name: 'app_add_to_cart')]
     public function index(EntityManagerInterface $entityManager, Product $product, Request $request): Response
     {
         $user = $this->getUser();
+
+
         $cart = new Cart();
         $cart->setUser($user);
         $cart->setCreatedAt(new \DateTime('NOW'));
@@ -36,16 +39,24 @@ class CartController extends AbstractController
         $cartItems->setQuantity($quantity);
 
         $entityManager->persist($cartItems);
+
         $entityManager->flush();
 
-        return $this->json(['quantity' => $cartItems->getQuantity()]);
+        $session = $request->getSession();
+        $cartSession = $session->get('cartSession', []);
+
+        if (empty($cartSession)) {
+            $session->set('cartSession', $quantity);
+        }
+
+        return $this->json(['quantity' => $cartItems->getQuantity(), 'cartSession' => $quantity]);
 
     }
 
     #[Route('cart', name: 'app_cart')]
-   public function cart(): Response
+   public function cart(Request $request): Response
    {
 
-       return new Response('Cart');
+       return new Response('');
    }
 }
